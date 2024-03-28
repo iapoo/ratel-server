196be22fe8cc9b32c71830435afec24ff6b86dd6
xiongbo.yang <xiongbo.yang@roadefend.com>
2024-03-28 17:27:16 +0800

    init

---
+++ ratel-rockie-common/src/main/java/org/ivipi/ratel/rockie/common/model/GoogleFontAxis.java
@@ -0,0 +1,16 @@
+package org.ivipi.ratel.rockie.common.model;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class GoogleFontAxis {
+    private static final long serialVersionUID = 330028891749330058L;
+
+    private String tag;
+
+    private String start;
+
+    private String end;
+}
